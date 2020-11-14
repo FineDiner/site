a2d09c9b97376174c7448638736355ed2462de12
Fine Diner <finediner0x00@gmail.com>
2020-11-14 10:26:58 -0800

    stuff

--- johnrieth/settings/production.py
+++ johnrieth/settings/production.py
@@ -1,6 +1,22 @@
 from .base import *
 
+import environ
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
+
+# False if not in os.environ
 DEBUG = False
+
+# Raises django's ImproperlyConfigured exception if SECRET_KEY not in os.environ
+SECRET_KEY = env('SECRET_KEY')
+
+# Parse database connection url strings like psql://user:pass@127.0.0.1:8458/db
+DATABASES = {
+    # read os.environ['DATABASE_URL'] and raises ImproperlyConfigured exception if not found
+    'default': env.db(),
+}
+
 
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
