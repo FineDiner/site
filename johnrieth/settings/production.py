2ebcec50e7193fdd14b2ca481259375912450819
Fine Diner <finediner0x00@gmail.com>
2020-11-14 10:22:15 -0800

    sentry

--- johnrieth/settings/production.py
+++ johnrieth/settings/production.py
@@ -2,6 +2,19 @@ from .base import *
 
 DEBUG = False
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://ffe427d4a03b4b67b430af8d7181b89f@o476455.ingest.sentry.io/5516116",
+    integrations=[DjangoIntegration()],
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
+
 try:
     from .local import *
 except ImportError:
